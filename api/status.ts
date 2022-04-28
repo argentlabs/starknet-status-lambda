7469d8d76d2823ee861154c0d03eca2bdf6e926c
Janek Rahrt <janek.rahrt@me.com>
2022-04-28 15:13:51 +0200

    feat: create cache proxy

---
+++ api/status.ts
@@ -0,0 +1,46 @@
+import type { VercelApiHandler } from "@vercel/node";
+import axios, { AxiosRequestHeaders } from "axios";
+
+const { HEADERS, ENDPOINT } = process.env;
+
+function parseHeaders(headers?: string): AxiosRequestHeaders | undefined {
+  try {
+    if (!headers) {
+      throw new Error("No headers provided");
+    }
+    return JSON.parse(headers);
+  } catch {
+    return undefined;
+  }
+}
+
+function validateEndpoint(endpoint?: string): endpoint is string {
+  return Boolean(endpoint);
+}
+
+const handler: VercelApiHandler = async (_request, response) => {
+  const isValidEndpoint = validateEndpoint(ENDPOINT);
+  if (!isValidEndpoint) {
+    response.status(500).send("No endpoint provided in ENV");
+    return;
+  }
+
+  try {
+    const headers = parseHeaders(HEADERS);
+    const { status, data } = await axios.get(ENDPOINT, {
+      headers,
+      validateStatus: () => true,
+      responseType: "text",
+    });
+
+    return response
+      .setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate=50") // Cache for 10 seconds and allow stale values to be served up to 50 seconds
+      .status(status)
+      .send(data);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).send("Error fetching endpoint");
+  }
+};
+
+export default handler;
