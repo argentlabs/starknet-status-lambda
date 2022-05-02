9c59b0ce876d884b766f49d16bbb672d2227689c
Janek Rahrt <janek.rahrt@me.com>
2022-05-02 15:17:23 +0200

    feat: add new `api/simple-status` endpoint

--- api/status.ts
+++ api/status.ts
@@ -1,29 +1,16 @@
 import type { VercelApiHandler } from "@vercel/node";
-import axios, { AxiosRequestHeaders } from "axios";
-import { allowCors } from "../lib/cors";
+import axios from "axios";
+import { allowCors } from "../middleware/cors";
+import { pipe } from "../middleware/pipe";
+import { swrCache } from "../middleware/swr";
+import { parseHeaders, validateEndpoint } from "../utils/env";
 
 const { HEADERS, ENDPOINT } = process.env;
-
-function parseHeaders(headers?: string): AxiosRequestHeaders | undefined {
-  try {
-    if (!headers) {
-      throw new Error("No headers provided");
-    }
-    return JSON.parse(headers);
-  } catch {
-    return undefined;
-  }
-}
-
-function validateEndpoint(endpoint?: string): endpoint is string {
-  return Boolean(endpoint);
-}
 
 const handler: VercelApiHandler = async (_request, response) => {
   const isValidEndpoint = validateEndpoint(ENDPOINT);
   if (!isValidEndpoint) {
-    response.status(500).send("No endpoint provided in ENV");
-    return;
+    return response.status(500).send("No endpoint provided in ENV");
   }
 
   try {
@@ -34,14 +21,11 @@ const handler: VercelApiHandler = async (_request, response) => {
       responseType: "text",
     });
 
-    return response
-      .setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate=50") // Cache for 10 seconds and allow stale values to be served up to 50 seconds
-      .status(status)
-      .send(data);
+    return response.status(status).send(data);
   } catch (error) {
     console.error(error);
     return response.status(500).send("Error fetching endpoint");
   }
 };
 
-export default allowCors(handler);
+export default pipe(allowCors, swrCache())(handler);
---
+++ middleware/pipe.ts
@@ -0,0 +1,4 @@
+export const pipe =
+  <T>(...fns: Array<(arg: T) => T>) =>
+  (value: T) =>
+    fns.reduce((acc, fn) => fn(acc), value);
